52abfa81860443a92fab13350f3b22f150e8a09f
jgmuchiri <jgmuchiri@gmail.com>
2019-02-02 00:23:16 -0500

    Update

--- application/views/apps/upload/view_file.php
+++
@@ -1,8 +0,0 @@
-<?php foreach ($images as $row) {
-    echo anchor('upload/view/'
-        . $row->file, '<img style="padding:5px" src="'
-        . base_url()
-        . 'assets/documents/uploads/'
-        . $row->file
-        . '" width="'.$imgWidth.'" height="'.$imgHeight.'"/></a>');
-}
